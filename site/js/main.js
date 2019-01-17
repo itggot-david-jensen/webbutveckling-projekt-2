6a0403ded55645f5c5e4a63da8ffee2f8a7c147c
itggot-david-jensen <david.jensen@elev.ga.ntig.se>
2019-01-17 15:37:22 +0100

    Worked with JS and SCSS

---
+++ site/js/main.js
@@ -0,0 +1,10 @@
+function menuopen(clickity){
+    let menu = document.querySelector(".menucontainer")
+    menu.classList.add("open")
+}
+
+function menuclose(clickity){
+    let menu = document.querySelector(".open")
+    menu.classList.remove("open")
+}
+
